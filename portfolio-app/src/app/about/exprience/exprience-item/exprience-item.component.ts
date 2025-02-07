c7a81efd371bce1f3e9e4056c35905720f6ffa5c
Alireza <a.z.nikravesh@gmail.comm>
2025-02-07 19:46:52 +0330

    completed about and expriences

---
+++ portfolio-app/src/app/about/exprience/exprience-item/exprience-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exprience-item',
+  imports: [],
+  templateUrl: './exprience-item.component.html',
+  styleUrl: './exprience-item.component.scss'
+})
+export class ExprienceItemComponent {
+
+}
