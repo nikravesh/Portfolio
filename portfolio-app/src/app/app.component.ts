7abcc66e18d65b4a4e651f6e759119bf1ee8535f
Alireza <a.z.nikravesh@gmail.comm>
2025-02-08 22:54:53 +0330

    added bootstrap classes

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
