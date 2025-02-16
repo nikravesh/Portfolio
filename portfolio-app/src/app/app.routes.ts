bc38082471c46bdb35875cad214f4768d1377a49
Alireza <a.z.nikravesh@gmail.comm>
2025-02-16 22:40:26 +0330

    added skills component

--- portfolio-app/src/app/app.routes.ts
+++ portfolio-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { BannerComponent } from './banner/banner.component';
+import { SkillsComponent } from './skills/skills.component';
 
 export const routes: Routes = [
   { path: '', component: BannerComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
 ];
