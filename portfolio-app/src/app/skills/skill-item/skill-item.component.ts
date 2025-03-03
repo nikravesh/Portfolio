26daa5668e21765e548096a2ec29001e46f04750
Alireza <a.z.nikravesh@gmail.comm>
2025-03-03 23:22:56 +0330

    developed skill items structure

---
+++ portfolio-app/src/app/skills/skill-item/skill-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-item',
+  imports: [],
+  templateUrl: './skill-item.component.html',
+  styleUrl: './skill-item.component.scss'
+})
+export class SkillItemComponent {
+
+}
--- portfolio-app/src/app/skills/skills.component.ts
+++ portfolio-app/src/app/skills/skills.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SkillItemComponent } from './skill-item/skill-item.component';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [SkillItemComponent],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {
-
-}
+export class SkillsComponent {}
