52cf668c0d042b28489d7feec8c52df462650fb2
a.nikravesh <a.nikravesh@dotin.ir>
2025-06-10 09:57:31 +0330

    updated banner and route to about page

--- portfolio-app/src/app/banner/banner.component.ts
+++ portfolio-app/src/app/banner/banner.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
-  imports: [FooterComponent],
+  imports: [FooterComponent, RouterLink],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
