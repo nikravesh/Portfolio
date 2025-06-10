23eed01e4756017ca2a39c8707defd4e767dfc4f
a.nikravesh <a.nikravesh@dotin.ir>
2025-06-10 10:12:24 +0330

    added router link in navbar

--- portfolio-app/src/app/about/about.component.ts
+++ portfolio-app/src/app/about/about.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ExprienceComponent } from './exprience/exprience.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { FooterComponent } from "../footer/footer.component";
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-about',
-  imports: [ExprienceComponent, AboutmeComponent, FooterComponent],
+  imports: [ExprienceComponent, AboutmeComponent, FooterComponent, NavBarComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
