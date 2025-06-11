b0aa2a189010bfbffd57717f5c81a546495090d9
Alireza <a.z.nikravesh@gmail.comm>
2025-06-11 10:01:32 +0330

    updated banner button links

--- portfolio-app/src/app/skills/skills.component.ts
+++ portfolio-app/src/app/skills/skills.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SkillItemComponent } from './skill-item/skill-item.component';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-skills',
-  imports: [SkillItemComponent],
+  imports: [SkillItemComponent, NavBarComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
