afab258dee4cbf2e6d9e9c09ee69ef1caab6cc5f
Alireza <a.z.nikravesh@gmail.comm>
2025-06-11 14:13:56 +0330

    refactored code

--- portfolio-app/src/app/services/skills.service.ts
+++ portfolio-app/src/app/services/skills.service.ts
@@ -5,41 +5,126 @@ import { SkillCategory } from '../skills/interfaces/skill-interface';
   providedIn: 'root',
 })
 export class SkillService {
+  getSkillsWithCategory(): SkillCategory[] {
+    return [
+      {
+        category: 'Frameworks',
+        skills: [
+          {
+            name: '.NET - .Net Core',
+            color: this.coloredSkillsByExprience(90),
+            level: 90,
+          },
 
-    getSkillsWithCategory():SkillCategory[] {
-        return [
-            {
-              category: 'Frameworks',
-              skills: [
-                { name: 'Angular', color: 'bg-red-500', level: 50 },
-                { name: 'React', color: 'bg-blue-500', level: 20 },
-                { name: 'Vue.js', color: 'bg-green-500', level: 10 },
-              ],
-            },
-            {
-              category: 'Backend',
-              skills: [
-                { name: 'Node.js', color: 'bg-green-700', level: 10 },
-                { name: '.NET Core', color: 'bg-blue-700', level: 60 },
-                { name: 'Express', color: 'bg-gray-700', level: 10 },
-              ],
-            },
-            {
-              category: 'Frontend',
-              skills: [
-                { name: 'HTML5', color: 'bg-orange-500', level: 80 },
-                { name: 'CSS3', color: 'bg-blue-300', level: 60 },
-                { name: 'Tailwind CSS', color: 'bg-teal-400', level: 40 },
-              ],
-            },
-            {
-              category: 'Tools',
-              skills: [
-                { name: 'Git', color: 'bg-purple-600', level: 65 },
-                { name: 'Docker', color: 'bg-blue-600', level: 30 },
-                { name: 'Jenkins', color: 'bg-red-600', level: 10 },
-              ],
-            },
-          ];   
-    }  
+          {
+            name: 'Entity Framework - EF Core',
+            color: this.coloredSkillsByExprience(90),
+            level: 90,
+          },
+          {
+            name: 'Angular',
+            color: this.coloredSkillsByExprience(60),
+            level: 60,
+          },
+          {
+            name: 'Tailwind',
+            color: this.coloredSkillsByExprience(45),
+            level: 45,
+          },
+        ],
+      },
+      {
+        category: 'Languages',
+        skills: [
+          {
+            name: 'C#',
+            color: this.coloredSkillsByExprience(90),
+            level: 60,
+          },
+          {
+            name: 'TypeScript',
+            color: this.coloredSkillsByExprience(50),
+            level: 10,
+          },
+          {
+            name: 'JavaScript',
+            color: this.coloredSkillsByExprience(25),
+            level: 10,
+          },
+        ],
+      },
+      {
+        category: 'Frontend',
+        skills: [
+          {
+            name: 'HTML',
+            color: this.coloredSkillsByExprience(80),
+            level: 80,
+          },
+          { name: 'CSS', color: this.coloredSkillsByExprience(60), level: 60 },
+          {
+            name: 'SCSS',
+            color: this.coloredSkillsByExprience(40),
+            level: 40,
+          },
+        ],
+      },
+      {
+        category: 'Database',
+        skills: [
+          {
+            name: 'SQL Server',
+            color: this.coloredSkillsByExprience(65),
+            level: 65,
+          },
+          {
+            name: 'MySQL',
+            color: this.coloredSkillsByExprience(65),
+            level: 65,
+          },
+          {
+            name: 'SQLite',
+            color: this.coloredSkillsByExprience(30),
+            level: 30,
+          },
+          {
+            name: 'Redis',
+            color: this.coloredSkillsByExprience(10),
+            level: 10,
+          },
+        ],
+      },
+      {
+        category: 'Tools',
+        skills: [
+          { name: 'Git', color: this.coloredSkillsByExprience(65), level: 65 },
+          {
+            name: 'Azure DevOps',
+            color: this.coloredSkillsByExprience(40),
+            level: 40,
+          },
+          {
+            name: 'Docker',
+            color: this.coloredSkillsByExprience(30),
+            level: 30,
+          },
+          {
+            name: 'RabbitMQ',
+            color: this.coloredSkillsByExprience(70),
+            level: 70,
+          },
+        ],
+      },
+    ];
+  }
+
+  private coloredSkillsByExprience(level: number): string {
+    if (level <= 30) {
+      return 'bg-yellow-500';
+    } else if (level <= 60) {
+      return 'bg-orange-500';
+    } else {
+      return 'bg-green-500';
+    }
+  }
 }
