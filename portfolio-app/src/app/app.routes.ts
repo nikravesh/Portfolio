f463e834a720c8e3f65deac363b7afa97c65def2
Alireza <a.z.nikravesh@gmail.comm>
2025-06-12 11:57:32 +0330

    added under-construction page

--- portfolio-app/src/app/app.routes.ts
+++ portfolio-app/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { AboutComponent } from './about/about.component';
 import { BannerComponent } from './banner/banner.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 export const routes: Routes = [
   { path: '', component: BannerComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'contact', component: ContactUsComponent },
+  { path: 'under-construction', component: UnderConstructionComponent },
+  { path: '**', component: UnderConstructionComponent },
 ];
---
+++ portfolio-app/src/app/under-construction/under-construction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+
+@Component({
+  selector: 'app-under-construction',
+  imports: [NavBarComponent],
+  templateUrl: './under-construction.component.html',
+  styleUrl: './under-construction.component.scss'
+})
+export class UnderConstructionComponent {
+
+}
