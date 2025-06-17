b975348ce9c4b3fe88dce01a2149c21627a1b04f
Alireza <a.z.nikravesh@gmail.comm>
2025-06-17 11:26:37 +0330

    updated exprience info

--- portfolio-app/src/app/services/exprience.service.ts
+++ portfolio-app/src/app/services/exprience.service.ts
@@ -10,9 +10,7 @@ export class ExperienceService {
       {
         company: 'Dotin Co. Ltd',
         description: [
-          'Maintained a corporate banking application and redesigned it using C#, Entity Framework, Hangfire, SQL Server, Domain-Driven Design (DDD) principles, resulting in a 30% improvement in application speed.',
-          'Optimized system performance while ensuring scalability and maintainability.•	Engineered a robust reporting solution for banks to produce precise PDF and Excel reports on customer loan balances and outstanding debts, leveraging C#, SQL Server, SQRS, and Stimulsoft.',
-          'Significantly accelerated the generation of large-scale bank reports from days to hours, resulting in a 45% increase in reporting efficiency.',
+          `I've been developing, maintaing, optimizing bunch of banking web application, services and APIs! Redesigned it using C#, Entity Framework, Dapper, Hangfire, SQL Server, Domain-Driven Design (DDD) principle.`,
         ],
         period: '2022 – Present',
         color: 'border-green-600 bg-green-600',
@@ -20,10 +18,7 @@ export class ExperienceService {
       {
         company: 'TosanTechno Co. Ltd',
         description: [
-          'Developed a real-time ATM monitoring system using C#, MySQL, and RabbitMQ that dramatically accelerated troubleshooting by 40%, leading to higher customer satisfaction and a 20% increase in company revenue.',
-          'Created an innovative monitoring solution that enabled remote identification of ATM malfunctions, resulting in a 35% faster resolution time and attracting new customers.',
-          'Analyzed ATM transaction data using a bank monitoring service to optimize ATM placement and maximize customer usage.',
-          'Increased customer adoption led to higher bank profits and a surge in demand from other banks, boosting company sales by 15%.',
+          'Developed a real-time ATM monitoring system using C#, MySQL, and RabbitMQ, that an innovative monitoring solution that enabled remote identification of ATM malfunctions.',
         ],
         period: '2018 – 2022',
         color: 'border-yellow-600 bg-red-600',
@@ -31,8 +26,7 @@ export class ExperienceService {
       {
         company: 'SAAT Co. Ltd',
         description: [
-          'Created a high-performance archival system for the Iranian Parliament using C#, Entity Framework, and SQL Server, resulting in a 50% increase in search speed.',
-          'Enhanced a complex archival system by implementing robust tagging and metadata management, resulting in a significant 25% increase in search speed and overall user satisfaction.',
+          'Created a high-performance archival system using C#, Entity Framework, and SQL Server. Enhanced a complex archival system by implementing robust tagging and metadata management',
           'Generated an automated radio playlist management system using C# and WPF, significantly accelerating the process of adding and preparing audio files for broadcast.',
         ],
         period: '2013 – 2018',
